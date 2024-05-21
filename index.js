e1ec1ef7be1f4ee44f977c72164cddbecfecf68c
eliklein02 <72769850+eliklein02@users.noreply.github.com>
2024-05-20 20:34:35 -0400

    mo

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Client } from 'whatsapp-web.js';
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize()
